d36e403b58d01f745232df02e34da5d7778660f8
TheadoreLee <theadorelee@TheadoreLeedeMacBook-Pro.local>
2021-07-11 18:49:39 +0800

    Add email notification

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import sys
 from helper import request_sail_ticket, create_assist_date
 from bark_helper import bark_push
 import time
+import smtplib
+import email.mime.multipart
+import email.mime.text
 
 # ==================================
 # Global Settings
@@ -16,19 +19,40 @@ enable_gh_action = False  # 启用GitHub Action
 # Bark Push
 enable_bark = False  # 启用Bark推送
 bark_token = ""  # Bark推送ID
+# SMTP config
+enable_mail = False  # 启用邮件推送
+smtp_url = ""
+smtp_port = 25
+smtp_from_address = ""
+smtp_to_address = ""
+smtp_password = ""
 # Ticket Stuff
 startSite = "SK"  # 始发站点
 endSite = "HKA"  # 目标站点
-startDate = "2021-08-1"  # 船票搜索日期
-endDate = "2021-08-30"
+startDate = "2021-09-14"  # 船票搜索日期
+endDate = "2021-09-14"
 show_available_only = True  # 只显示有票的日期
 # ==================================
+
+def sendMail(mailContent):
+    msg = email.mime.multipart.MIMEMultipart()
+    msg['from'] = smtp_from_address
+    msg['subject'] = '船票Get'
+    msg['to'] = smtp_to_address
+    txt=email.mime.text.MIMEText(mailContent,'HTML','utf-8')
+    msg.attach(txt)
+    smtp.sendmail(msg['from'],msg['to'],str(msg))
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "--action":
         enable_gh_action = True  # 检测是否在GitHub Action中运行
 
     toDate = create_assist_date(datestart=startDate, dateend=endDate)  # 船票的起止时间
+
+    smtp = smtplib
+    smtp = smtplib.SMTP()
+    smtp.connect(smtp_url, smtp_port)
+    smtp.login(smtp_from_address, smtp_password)
 
     while True:
         for date in toDate:
@@ -40,12 +64,15 @@ if __name__ == '__main__':
                 show_available_only=show_available_only)
             if ret is not None and ret != -1:
                 print(f'{date}:{ret}')
+                if enable_mail:
+                    sendMail(f'{date}:{ret}')
                 if enable_bark:
                     bark_push(token=bark_token, title="船票Get", content=ret)
             elif ret == -1:
                 print(f'{date}:爬取失败')
             else:
                 print(f'{date}:没有可用的船票')
+
         if enable_gh_action:
             break
         time.sleep(600)
